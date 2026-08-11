c7c5c9a07ea35d6a1a2c88fedbf920e829f200e6
williansouzarobert-eng <willian.souza.robert@escola.pr.gov.br>
2026-08-11 09:42:03 -0300

    s

---
+++ script.js
@@ -0,0 +1,18 @@
+</head>
+  <body>
+      <header>
+       <h1>meu  blog tech</h1>
+       <p>vou compartilhar conhecimento sobre vendas de imovel</p>
+      </header>
+      </main>
+       <h2>meu primeiro post</h2>
+       <p>por: robert willian souza</p>
+       <p> boas-vindas ao meu novo blog aqui vou compartilhar dicas de 
+        como vender mais rapido </p>
+        
+    </body>
+ </html>
+ 
+
+ <header></header>
+
